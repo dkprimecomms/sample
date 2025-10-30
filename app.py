f0006a7d8d5f37071bbf5bf8e2bc352c7016d7f6
dkprimecomms <dkanna@primecomms.com>
2025-10-30 11:31:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello from App Runner! ✅", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
